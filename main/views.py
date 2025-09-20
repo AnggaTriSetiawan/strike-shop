8beba70c24b1cd1ab99ec6f9050a303a9a1018e5
AnggaTriSetiawan <anggatrisetiawan81@gmail.com>
2025-09-20 13:58:08 +0700

    Memperbaiki nama class Item pada models.py menjadi Product

--- main/views.py
+++ main/views.py
@@ -1,63 +1,63 @@
 from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render, redirect, get_object_or_404
-from main.forms import ItemForm
-from main.models import Item
+from main.forms import ProductForm
+from main.models import Product
 
 def show_main(request):
-    item_list = Item.objects.all()
+    product_list = Product.objects.all()
     context = {
         'app_name' : 'Strike Shop',
         'name': 'Angga Tri Setiawan',
         'class': 'PBP F',
-        'item_list': item_list
+        'product_list': product_list
     }
 
     return render(request, "main.html", context)
 
-def create_item(request):
-    form = ItemForm(request.POST or None)
+def create_product(request):
+    form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
         form.save()
         return redirect('main:show_main')
 
     context = {'form': form}
-    return render(request, "create_item.html", context)
+    return render(request, "create_product.html", context)
 
-def show_item(request, id):
-    item = get_object_or_404(Item, pk=id)
-    item.increment_views()
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product.increment_views()
 
     context = {
-        'item': item
+        'product': product
     }
 
-    return render(request, "item_detail.html", context)
+    return render(request, "product_detail.html", context)
 
 def show_xml(request):
-    item_list = Item.objects.all()
-    xml_data = serializers.serialize("xml", item_list)
+    product_list = Product.objects.all()
+    xml_data = serializers.serialize("xml", product_list)
     return HttpResponse(xml_data, content_type="application/xml")
 
 def show_json(request):
-    item_list = Item.objects.all()
-    json_data = serializers.serialize("json", item_list)
+    product_list = Product.objects.all()
+    json_data = serializers.serialize("json", product_list)
     return HttpResponse(json_data, content_type="application/json")
 
-def show_xml_by_id(request, item_id):
+def show_xml_by_id(request, product_id):
     try:
-        item_list = Item.objects.filter(pk=item_id)
-        xml_data = serializers.serialize("xml", item_list)
+        product_list = Product.objects.filter(pk=product_id)
+        xml_data = serializers.serialize("xml", product_list)
         return HttpResponse(xml_data, content_type="application/xml")
-    except Item.DoesNotExist:
+    except Product.DoesNotExist:
         return HttpResponse(status=404)
 
-def show_json_by_id(request, item_id):
+def show_json_by_id(request, product_id):
     try:
-        item_list = Item.objects.get(pk=item_id)
-        json_data = serializers.serialize("json", [item_list])
+        product_list = Product.objects.get(pk=product_id)
+        json_data = serializers.serialize("json", [product_list])
         return HttpResponse(json_data, content_type="application/json")
-    except Item.DoesNotExist:
+    except Product.DoesNotExist:
         return HttpResponse(status=404)
 
