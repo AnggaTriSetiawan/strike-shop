579b39706f28fa96689c770912149fdde3f7269d
AnggaTriSetiawan <anggatrisetiawan81@gmail.com>
2025-09-20 14:31:18 +0700

    Membuat fitur autentikasi user untuk register dan login

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -9,6 +10,8 @@ class Product(models.Model):
         ('topi', 'Topi'),
         ('badge', 'Badge')
     ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     name = models.CharField(max_length=100, default="Empty")
     price = models.PositiveIntegerField(default=0)
     description = models.TextField(default="Empty")
